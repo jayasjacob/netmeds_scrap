a674318f14b81ec592c064ec7ea37f3824d56ea2
Jayas P Jacob <37359731+thedevboy@users.noreply.github.com>
2022-03-22 19:43:20 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 source /home/jayas/Documents/Projects/netmeds_scraping/venv/bin/activate  #path to your venv
-export PYTHONPATH='/home/jayas/Documents/Projects/netmeds_scraping/'
+export PYTHONPATH='/home/jayas/Documents/Projects/netmeds_scraping/'  #path to your project root
 scrapy crawl netmeds -o data.json
